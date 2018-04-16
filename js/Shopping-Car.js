7c5d17dda46a08a51b453a5eb2416c0fd96c1643
JeremyNine <1626326986@qq.com>
2018-04-16 22:11:33 +0800

    add new vue
    use v-for to build the html

--- js/Shopping-Car.js
+++ js/Shopping-Car.js
@@ -1,18 +1,25 @@
 /**
  * Created by Admin on 2018/4/15.
  */
-new Vue({
+var cm =new Vue({
     el:"#app",
     data:{
-
+        totleMoney:0,
+        productList:[]
     },
     filters:{
 
     },
     mounted: function (){
-
+        this.cartview();
     },
     methods:{
-
+        cartview: function () {
+            var _this=this;
+            this.$http.get("./data/cart.json",{"id":123}).then(function(res){
+                _this.productList=res.body.result.list;
+                _this.totleMoney=res.body.result.totleMoney;
+            })
+        }
     }
 });
